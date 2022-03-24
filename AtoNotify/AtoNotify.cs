697478db1e98222106a2cafed3c605faa66717a6
kjurlina <kjurlina@HRATOMX8160>
2022-03-24 15:02:31 +0100

    Add project files.

---
+++ AtoNotify/AtoNotify.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace AtoNotify
+{
+    internal class AtoNotify
+    {
+        // Ovaj komad koda prima dva argumenta na ulaz: vremenski žig i tekst poruke
+        // Od te dvije informacije sastavlja se poruka koja se šalje klijentu preko notify@ato.hr računa
+        // Imati na umu da aplikacija nije definirana kao konzolaška - to jest nema niti jednog vidljivog prozora
+        // To se može promijeniti u property sheetu aplikacije - odabrati "Console application"
+        // Coding by kjurlina. Have a lot of fun.
+        static void Main(string[] args)
+        {
+            // Deklaracija varijabli
+            AtoNotifySupervisor Supervisor = null;
+            string date = string.Empty;
+            string time = string.Empty;
+            string message = string.Empty;
+            int i;
+
+            // Instanca supervizora
+            try
+            {
+                Supervisor = new AtoNotifySupervisor();
+                AppDomain.CurrentDomain.ProcessExit += new EventHandler((sender, e) => CurrentDomain_ProcessExit(sender, e, Supervisor));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            // Kreiranje datoteke za logiranje
+            try
+            {
+                if (!Supervisor.CheckLogFileExistence())
+                {
+                    // Create log file
+                    Supervisor.CreateLogFile();
+                }
+                else if (Supervisor.ChecklLogFileSize() > 1048576)
+                {
+                    // If log file is too big archive it and create new one
+                    Supervisor.ArchiveLogFile();
+                    Supervisor.CreateLogFile();
+                }
+                else
+                {
+                    // Zasad ne zapisujemo kada se aplikacija pokrece i zaustavlja
+                    // Supervisor.ToLogFile("Application started with existing log file");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Something went wrong with log file. Exiting application");
+                return;
+            }
+
+            // Procitaj ulazne argumente. Mora biti minimalno 3 argumenta: datum, vrijeme i tekst alarma
+            try
+            {
+                date = args[0];
+                time = args[1];
+                for (i = 2; i < args.Length; i++)
+                {
+                    message = message + args[i] + " ";
+                }
+            }
+            catch (Exception ex)
+            {
+                Supervisor.ToLogFile("Aplikacija nije pozvana s ispravnim parametrima (string datum + string vrijeme + string tekst odvojeni razmakom...)");
+                Supervisor.ToLogFile(ex.Message);
+            }
+
+            // Ukoliko su ulazni parametri OK, kreiraj SMTP klijenta i posalji poruku
+            try
+            {
+                // Definiraj SMPT klijenta
+                SmtpClient AtoSMTP = new SmtpClient("smtp.office365.com");
+
+                // Definiraj kredencijali
+                AtoSMTP.UseDefaultCredentials = false;
+                NetworkCredential basicAuthenticationInfo = new NetworkCredential("notifier@ato.hr", "Xax75901");
+                AtoSMTP.Credentials = basicAuthenticationInfo;
+                AtoSMTP.EnableSsl = true;
+
+                // Kreiraj poruku
+                MailAddress from = new MailAddress("notifier@ato.hr", "ATO Notifikator");
+                MailAddress to = new MailAddress("gigabajt@gmail.com", "Kruni");
+                MailMessage msg = new MailMessage(from, to);
+
+                msg.Subject = "ATO Notifikator :: CNUS šalje poruku :: " + message + " :: " + date + " " + time;
+                msg.SubjectEncoding = msg.BodyEncoding = Encoding.UTF8;
+                msg.IsBodyHtml = true;
+                msg.Body = "Tekst alarma :: " + message + " <br />" + "Vremenski žig alarma :: " + date + " " + time;
+
+                // Pošalji poruku
+                AtoSMTP.Send(msg);
+
+                // Napravi zapis
+                Supervisor.ToLogFile("Poslana poruka :: " + message);
+            }
+
+            // Handlanje grešaka - samo ispiši u konzolu i izađi van
+            catch (SmtpException ex)
+            {
+                Supervisor.ToLogFile("SMPT exception :: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Supervisor.ToLogFile("General exception :: " + ex.Message);
+            }
+        }
+
+        // Izlazna strategija...
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e, AtoNotifySupervisor supervisor)
+        {
+            // Zasad ne zapisujemo kada se aplikacija pokrece i zaustavlja
+            // Samo greske i uspjesno poslane mailove (gore u kodu)
+            // supervisor.ToLogFile("Application closed");
+            // supervisor.ToLogFile("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+}
---
+++ AtoNotify/AtoNotifySupervisor.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtoNotify
+{
+    internal class AtoNotifySupervisor
+    {
+        // Global class variables
+        private string ExePath;
+        private string ConfigDirPath;
+        private string ConfigFilePath;
+        private string LogDirPath;
+        private string LogFilePath;
+        private string SeparationString;
+
+        private string[] ConfigFileContent;
+        private string[] ConfigLineContent;
+        private int ConfigFileNumberOfLines;
+        private bool Verbose = false;
+
+        int i;
+
+        // Constructor
+        public AtoNotifySupervisor()
+        {
+            // Get executable path
+            ExePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Create configuration & log file names & paths
+            if (Environment.OSVersion.ToString().Contains("Windows"))
+            {
+                ConfigDirPath = ExePath + @"config";
+                LogDirPath = ExePath + @"logs";
+                ConfigFilePath = ConfigDirPath + @"\AtoNotifyConfig.txt";
+                LogFilePath = LogDirPath + @"\AtoNotifyLog.txt";
+                SeparationString = " :: ";
+            }
+            else if (Environment.OSVersion.Platform.ToString().Contains("Linux"))
+            {
+                ConfigDirPath = ExePath + @"config";
+                LogDirPath = ExePath + @"logs";
+                ConfigFilePath = ConfigDirPath + @"\AtoNotifyConfig.txt";
+                LogFilePath = LogDirPath + @"\AtoNotifyLog.txt";
+                SeparationString = " :: ";
+            }
+            else
+            {
+                ConfigFilePath = "";
+                LogFilePath = "";
+                SeparationString = "";
+            }
+        }
+
+        // Log file functions
+        public bool CheckLogFileExistence()
+        {
+            // Check if config file exists
+            return File.Exists(LogFilePath);
+        }
+
+        public long ChecklLogFileSize()
+        {
+            // Check log file size
+            long LogFileSize = new FileInfo(LogFilePath).Length;
+            return LogFileSize;
+        }
+
+        public void CreateLogFile()
+        {
+            Directory.CreateDirectory(LogDirPath);
+            var fileStream = File.Create(LogFilePath);
+            fileStream.Close();
+            // Zasad ne zapisujemo kada se aplikacija pokrece i zaustavlja
+            // ToLogFile("Application has started with new log file");
+        }
+
+        public void ArchiveLogFile()
+        {
+            // Put some info into existing file (the one that will be archived)
+            ToLogFile("Log file size is too big. It will be archived and new one will be created");
+
+
+            // Save file and extend name with current timestamp
+            string ArchiveLogFilePath;
+            string LogFileTS = DateTime.Now.Year.ToString() + "_" +
+                               DateTime.Now.Month.ToString() + "_" +
+                               DateTime.Now.Day.ToString() + "_" +
+                               DateTime.Now.Hour.ToString() + "_" +
+                               DateTime.Now.Minute.ToString() + "_" +
+                               DateTime.Now.Second.ToString();
+
+            ArchiveLogFilePath = LogDirPath + @"\AtoNotifyLog_" + LogFileTS + ".txt";
+
+            // Create current log file archive copy
+            File.Copy(LogFilePath, ArchiveLogFilePath);
+            File.Delete(LogFilePath);
+        }
+
+        // Configuration file functions
+        public bool CheckConfigFileExistence()
+        {
+            // Check if config file exists
+            return File.Exists(ConfigFilePath);
+        }
+
+        public bool ReadConfigFile()
+        {
+            // This method to read configuration file content
+            ConfigFileContent = File.ReadAllLines(ConfigFilePath);
+
+            if (ConfigFileContent.Length > 0)
+            {
+                // Get number of configuration file lines
+                ConfigFileNumberOfLines = File.ReadAllLines(ConfigFilePath).Length;
+                // Read verbose mode
+                Verbose = GetVerboseMode();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool GetVerboseMode()
+        {
+            // This method to get verbose mode from config file content
+            try
+            {
+                i = 0;
+                while (i <= ConfigFileNumberOfLines)
+                {
+                    if (i >= ConfigFileNumberOfLines)
+                    {
+                        ToLogFile("There is nothing about verbose mode in configuration file. We will assume it's off");
+                        return false;
+                    }
+
+                    ConfigLineContent = ConfigFileContent[i].Split(SeparationString.ToCharArray());
+                    if (ConfigLineContent[0] == "Verbose")
+                    {
+                        ToConsole("Verbose mode is on. All future messages will be mirrored to console");
+                        ToLogFile("Verbose mode is on. All future messages will be mirrored to console");
+                        return true;
+                    }
+                    i++;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                ToLogFile("Something went wrong with determination of verbose mode. We will assume it's off");
+                ToLogFile(ex.Message);
+                return false;
+            }
+        }
+
+        // Logging functions
+        public void ToConsole(string message)
+        {
+            // Output message to console
+            string MessageTS = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff");
+            Console.WriteLine(MessageTS + " :: " + message);
+        }
+
+        public void ToLogFile(string message)
+        {
+            // Output message to log file
+            string MessageTS = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff");
+            using (StreamWriter sw = File.AppendText(LogFilePath))
+            {
+                sw.WriteLine(MessageTS + " :: " + message);
+                if (Verbose)
+                {
+                    Console.WriteLine(MessageTS + " :: " + message);
+                }
+            }
+        }
+    }
+}
